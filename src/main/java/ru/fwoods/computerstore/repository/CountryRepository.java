3a94416ac58afa9da177a633fb8cffc02854c94f
Frank-Woods <49346787+Frank-Woods@users.noreply.github.com>
2020-12-02 23:40:49 +0400

    update

--- src/main/java/ru/fwoods/computerstore/repository/CountryRepository.java
+++ src/main/java/ru/fwoods/computerstore/repository/CountryRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.fwoods.computerstore.domain.Country;
 
+import java.util.List;
+
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Long> {
     Country getCountryByName(String name);
+
+    List<Country> findAllByNameContainsIgnoreCase(String searchParam);
 }
