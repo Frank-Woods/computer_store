145af6bf97c7ee008516d5ecc8d7338aa6511096
Frank-Woods <49346787+Frank-Woods@users.noreply.github.com>
2020-12-03 14:50:51 +0400

    update promotion

--- src/main/java/ru/fwoods/computerstore/controller/PromotionController.java
+++ src/main/java/ru/fwoods/computerstore/controller/PromotionController.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import ru.fwoods.computerstore.domain.Promotion;
+import ru.fwoods.computerstore.domain.PromotionProduct;
+import ru.fwoods.computerstore.model.DiscountProduct;
 import ru.fwoods.computerstore.service.ImageService;
+import ru.fwoods.computerstore.service.PromotionProductService;
 import ru.fwoods.computerstore.service.PromotionService;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -18,6 +22,9 @@ public class PromotionController {
 
     @Autowired
     private ImageService imageService;
+
+    @Autowired
+    private PromotionProductService promotionProductService;
 
     @GetMapping("/admin/promotion/create")
     public String getPromotionPage() {
@@ -30,10 +37,31 @@ public class PromotionController {
             Map<String, Object> model
     ) {
         Promotion promotion = promotionService.findById(id);
-        String banner = imageService.getImageByPromotionId(id);
 
         model.put("promotion", promotion);
-        model.put("banner", banner);
+        model.put("banner", promotion.getBanner().getFilename());
         return "admin/promotion/update";
+    }
+
+    @GetMapping("/admin/promotion/${id}/product/all")
+    public String getPromotionProductPage(
+            @PathVariable Long id,
+            Map<String, Object> model
+    ) {
+        List<DiscountProduct> promotionProducts = promotionProductService.getAllByPromotionId(id);
+
+        model.put("products", promotionProducts);
+        return "admin/promotion/product/all";
+    }
+
+    @GetMapping("/admin/promotion/product/update/${id}")
+    public String getPromotionProductUpdate(
+            @PathVariable Long id,
+            Map<String, Object> model
+    ) {
+        DiscountProduct discountProduct = promotionProductService.getDiscountProductById(id);
+
+        model.put("product", discountProduct);
+        return "admin/promotion/product/update";
     }
 }
--- src/main/java/ru/fwoods/computerstore/service/PromotionProductService.java
+++ src/main/java/ru/fwoods/computerstore/service/PromotionProductService.java
@@ -3,7 +3,11 @@ package ru.fwoods.computerstore.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.fwoods.computerstore.domain.PromotionProduct;
+import ru.fwoods.computerstore.model.DiscountProduct;
 import ru.fwoods.computerstore.repository.PromotionProductRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PromotionProductService {
@@ -16,5 +20,30 @@ public class PromotionProductService {
 
     public void delete(PromotionProduct promotionProduct) {
         promotionProductRepository.delete(promotionProduct);
+    }
+
+    public List<DiscountProduct> getAllByPromotionId(Long id) {
+        List<PromotionProduct> promotionProducts = promotionProductRepository.getAllByPromotionId(id);
+
+        return promotionProducts.stream().map(promotionProduct -> {
+            DiscountProduct discountProduct = new DiscountProduct();
+
+            discountProduct.setProduct(promotionProduct.getProductData().getId());
+            discountProduct.setName(promotionProduct.getProductData().getName());
+            discountProduct.setDiscount(promotionProduct.getDiscount());
+
+            return discountProduct;
+        }).collect(Collectors.toList());
+    }
+
+    public DiscountProduct getDiscountProductById(Long id) {
+        PromotionProduct promotionProduct = promotionProductRepository.getOne(id);
+
+        DiscountProduct discountProduct = new DiscountProduct();
+        discountProduct.setProduct(promotionProduct.getProductData().getId());
+        discountProduct.setName(promotionProduct.getProductData().getName());
+        discountProduct.setDiscount(promotionProduct.getDiscount());
+
+        return discountProduct;
     }
 }
