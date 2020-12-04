77ee0f6865b2c626e702fb943ffeaefe980d2cbc
Shevyakov Vlad <v.shevy@ya.ru>
2020-12-05 00:28:58 +0400

    upd

--- src/main/java/ru/fwoods/computerstore/controller/UserController.java
+++ src/main/java/ru/fwoods/computerstore/controller/UserController.java
@@ -57,7 +57,7 @@ public class UserController {
         Pageable pageable = PageRequest.of(page, 15);
         Page<Review> reviews = reviewService.getReviewsByUser(user, pageable);
         model.put("reviewsPage", reviews);
-        return "site/user/profile/review";
+        return "site/user/profile/reviews";
     }
 
     @GetMapping("/profile/reviews/{id}")
