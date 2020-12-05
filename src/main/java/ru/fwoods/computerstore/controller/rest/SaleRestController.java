4dd9a2beb2c6ec4e58049ab5a0bf506f685a96fd
Frank-Woods <49346787+Frank-Woods@users.noreply.github.com>
2020-12-05 21:46:05 +0400

    update

--- src/main/java/ru/fwoods/computerstore/controller/rest/SaleRestController.java
+++ src/main/java/ru/fwoods/computerstore/controller/rest/SaleRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import ru.fwoods.computerstore.model.Sale;
+import ru.fwoods.computerstore.model.SaleProduct;
 import ru.fwoods.computerstore.service.SaleService;
 
 @RestController
@@ -19,9 +20,9 @@ public class SaleRestController {
     @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping(value = "/admin/sale/update", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity updateSale(
-            @RequestPart(name = "sale") Sale sale
+            @RequestPart(name = "sale") SaleProduct saleProduct
     ) {
-        saleService.saleUpdate(sale);
+        saleService.saleUpdate(saleProduct);
         return ResponseEntity.ok().body(null);
     }
 }
