379e9f954407e9b4c69c49a2bc7eead0f783b892
Shevyakov Vlad <v.shevy@ya.ru>
2020-12-05 16:56:37 +0400

    upd

--- src/main/java/ru/fwoods/computerstore/controller/TestController.java
+++ src/main/java/ru/fwoods/computerstore/controller/TestController.java
@@ -10,4 +10,9 @@ public class TestController {
     public String test() {
         return "site/cart/index";
     }
+
+    @GetMapping("/cart/payment")
+    public String test2() {
+        return "site/cart/payment";
+    }
 }
