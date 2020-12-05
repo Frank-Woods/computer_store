e8036e0c32e2227aeb5fcbbbafdf15e096f01963
Frank-Woods <49346787+Frank-Woods@users.noreply.github.com>
2020-12-05 20:57:13 +0400

    update

--- src/main/java/ru/fwoods/computerstore/controller/TestController.java
+++ src/main/java/ru/fwoods/computerstore/controller/TestController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-    @GetMapping("/cart")
-    public String test() {
-        return "site/cart/index";
-    }
-//
-//    @GetMapping("/cart/payment")
-//    public String test2() {
-//        return "site/cart/payment";
+//    @GetMapping("/cart")
+//    public String test() {
+//        return "site/cart/index";
 //    }
+////
+////    @GetMapping("/cart/payment")
+////    public String test2() {
+////        return "site/cart/payment";
+////    }
 }
